0e43acbc420a9ea5fe30e2c7b1f9cb202dd22741
Eknaprasath Padmaraj <EP00606284@techmahindra.com>
2020-08-20 15:49:12 +0530

    variable added

--- variables.tf
+++ variables.tf
@@ -8,9 +8,21 @@ variable "lambda_name" {
   default     = "CW-Alarm-Creation"
   description = "lamda function name"
 }
-
 variable "sns_arn" {
   type        = string
   default     = "arn:aws:sns:us-east-1:356143132518:cloudwatch_alarm"
-  description = "description"
+  description = "SNS ARN which will be used to trigger notification by CloudWatch"
+}
+variable "cw_threshold" {
+  type        = number
+  default     = "80"
+  description = "Threshold for CPU,Memory & Disk Utilization"
+}
+variable "AWS_ACCESS_KEY" {
+  type        = string
+  description = "Access Key"
+}
+variable "SECRET_ACCESS_KEY" {
+  type        = string
+  description = "Secret Access Key"
 }
